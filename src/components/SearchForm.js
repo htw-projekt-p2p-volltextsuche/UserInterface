afd59c6602f3bf27169c3bc567e512548e20a49e
Simon <s0568782@htw-berlin.de>
2021-06-01 20:27:49 +0200

    decided on query and result data structure
     Co-authored-by: jduesentrieb no-reply

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,7 @@
 import AddSearchButton from "./AddSearchButton";
 import SearchSelect from "./selects/SearchSelect";
 import SubmitButton from "./SubmitButton";
-
+import RegexSelect from "./selects/RegexSelect";
 function SearchForm(params) {
   return (
     <div className="formContainer">
@@ -9,6 +9,14 @@ function SearchForm(params) {
         <SearchSelect />
         <input type="text" />
         <br/>
+        <RegexSelect/>
+        <SearchSelect />
+        <input type="text" />
+
+        <RegexSelect/>
+        <SearchSelect />
+        <input type="text" />
+
         <AddSearchButton/>
         <SubmitButton />
       </form>
