79219f2ec7e4fc571210d83d736bc9cbffc7356d
Bresicode <s0568782@htw-berlin.de>
2021-07-15 05:44:53 +0200

    added cors to backend app

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const protocolRouter = require("./routes/protocol-router")
-
+var cors = require('cors')
 
 const app = express()
 const apiPort = 3001
-
+app.use(cors())
 app.use('/api', protocolRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
